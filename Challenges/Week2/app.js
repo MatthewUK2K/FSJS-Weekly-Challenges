e8ac7fa0a195bdfd6cff21df1a61e93e95137e53
Matthew Brandenburg <matthewbrandenburg@dhcp012195.code-louisville-wifi.lmcdc.local>
2016-09-08 19:47:16 -0400

    completeing the weekly challenges

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,20 @@
 //create function
   //control and return statements
 
-//Prompt user for input and store variable
+var input = prompt("Hello to You!")//Prompt user for input and store variable
+//if (input.toLowerCase() == "hello"){
+//  alert("Hello World!");
+//} else {
+  //prompt("That's not what I was looking for.  Try again.");
+//}
+function processInput(message){
 
-//Call function
+  if (message == "hello") {
+    return("Hello world");
+  } else {
+    return("You didnt say hello.");
+  }
 
-//Alert user results
+}
+var response = processInput(input);
+console.log(response);
